991b97de83480fdec345db9c4f3f2610e5f4e9a4
robbbbor <rbbwilwand@gmail.com>
2025-03-01 20:43:30 -0800

    githubactions

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  basePath: "/nextjs-mothership-terminal",
+  images: {
+    unoptimized: true, 
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
